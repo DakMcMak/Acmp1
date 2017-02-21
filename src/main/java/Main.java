3d5f3c465a14165e1cf0cce396ee7b7861f3217c
DakMcMak <esbolov.galymzhan@mail.ru>
2017-02-21 21:29:43 +0600

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        int number1 = Scanner.nextInt();
+        int number2 = Scanner.nextInt();
+        int number3;
+        number3 = number1 + number2;
+        System.out.println(number3);
+    }
+}
